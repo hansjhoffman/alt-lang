9f1f35c2ee16552eebebec2a310ef5e17664193e
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-08-29 11:13:15 -0400

    add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Data.Text (Text)
+import Test.Hspec
+import Test.Hspec.Megaparsec
+import Text.Megaparsec (runParser)
+
+import Lexer
+
+
+-- Helpers
+
+
+shouldMatch :: (Show a, Eq a) => Parser a -> Text -> a -> IO ()
+shouldMatch parser input result =
+  runParser parser "" input `shouldBe` (Right result)
+
+
+-- Tests
+
+
+integerSpec :: Spec
+integerSpec = 
+  describe "Integer" $ do
+    it "should handle valid integer with no space" $
+      shouldMatch integer "42" 42
+      
+    -- it "should handle valid integer with space" $
+    --   shouldMatch integer " 42" 42
+    
+    
+floatSpec :: Spec
+floatSpec =
+  describe "Float" $ do
+    it "should handle valid float with no space" $
+      shouldMatch float "42.0" 42.0
+
+
+-- Main
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = 
+  hspec $ do
+    integerSpec
+    floatSpec
