a59d61a7c58c565ed37ecfce3672b9c3b24e9e77
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-08-29 12:08:42 -0400

    add more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -26,6 +26,9 @@ integerSpec =
     describe "Integer" $ do
         it "should handle valid integer with no space" $
             shouldMatch integer "42" 42
+            
+        it "should handle signed integers" $
+            shouldMatch signedInteger "-42" (-42)
       
         -- it "should handle valid integer with space" $
         --     shouldMatch integer " 42" 42
@@ -36,6 +39,16 @@ floatSpec =
     describe "Float" $ do
         it "should handle valid float with no space" $
             shouldMatch float "42.0" 42.0
+            
+        it "should handle signed floats" $
+            shouldMatch signedFloat "-42.0" (-42.0)
+
+
+stringLiteralSpec :: Spec
+stringLiteralSpec =
+    describe "String Literal" $ do
+        it "should handle valid strings" $
+            shouldMatch stringLiteral "\"hello world\"" "hello world"
 
 
 -- Main
@@ -46,3 +59,4 @@ main =
     hspec $ do
         integerSpec
         floatSpec
+        stringLiteralSpec
