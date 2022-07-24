5b4779574cfab51eec112420f273b1783bce7904
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-07-24 15:47:01 -0600

    add nix, brittany and other configs

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for alt
-
-## Unreleased changes
---
+++ shell.nix
@@ -0,0 +1,20 @@
+let
+  sources = import ./nix/sources.nix { };
+  pkgs = import sources.nixpkgs { };
+
+  haskellDeps = ps: with ps; [ base hspec ];
+
+  ghc = pkgs.haskell.compiler.ghc922 haskellDeps;
+
+  inputs = [ pkgs.gcc pkgs.ghc pkgs.ghcid pkgs.stack pkgs.llvm pkgs.nixfmt ];
+
+  hooks = ''
+    mkdir -p .nix-stack
+    export STACK_ROOT=$PWD/.nix-stack
+  '';
+in pkgs.stdenv.mkDerivation {
+  name = "alt";
+  src = ./.;
+  buildInputs = inputs;
+  shellHook = hooks;
+}
